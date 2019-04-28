2a576d7d0ec4be33a1965f91121f1290b4c02d90
CRVARUN <49754981+CRVARUN@users.noreply.github.com>
2019-04-28 21:47:03 +0530

    Update wieC.js

--- components/PastEvents/wieC.js
+++ components/PastEvents/wieC.js
@@ -10,61 +10,54 @@ import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 
-export const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-export const tutorialSteps = [
+const tutorialSteps = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
+    label: 'WIE SYMPOSIUM 2019',
     imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+      'https://drive.google.com/uc?export=download&id=1P0BpbANiG-Rn5Qd65yhviKNdr54KCYK7',
   },
   {
-    label: 'Bird',
+    label: 'WIE SYMPOSIUM 2019',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+      'https://drive.google.com/uc?export=download&id=1XLobVuIyk-CcDJAxPR8WMC5-StFGYknl',
   },
   {
-    label: 'Bali, Indonesia',
+    label: 'WIE SYMPOSIUM 2019',
     imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+      'https://drive.google.com/uc?export=download&id=12ZMloAsvwU6BWRqICIrIAwEWrAll0dF2',
   },
   {
-    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
+    label: 'WIE SYMPOSIUM 2019',
     imgPath:
-      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
+      'https://drive.google.com/uc?export=download&id=1tJHjKyYt5bVPQBAZW32KIqreiRzvn3St',
   },
   {
-    label: 'Goč, Serbia',
+    label: 'WIE SYMPOSIUM 2019',
     imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+      'https://drive.google.com/uc?export=download&id=18vABsznYv58mC3Lxp2iBaYOc59SU_UKp',
   },
+  
+  
 ];
 
-export const styles = theme => ({
+const styles = theme => ({
   root: {
-    margin:'0',
-    padding:'0',
-    maxWidth: 800,
+    maxWidth: 400,
     flexGrow: 1,
-    flexShrink:1
   },
   header: {
-    display: 'block',
+    display: 'flex',
     alignItems: 'center',
-    height: 120,
-    paddingTop: '20px',
+    height: 50,
     paddingLeft: theme.spacing.unit * 4,
-    paddingRight: theme.spacing.unit * 4,
     backgroundColor: theme.palette.background.default,
-  },
-  imb:{
-    paddingRight:0,
-    width:'100%',
   },
   img: {
     height: 255,
     display: 'block',
-    maxWidth: 800,
+    maxWidth: 400,
     overflow: 'hidden',
     width: '100%',
   },
@@ -98,7 +91,9 @@ class SwipeableTextMobileStepper extends React.Component {
 
     return (
       <div className={classes.root}>
-
+        <Paper square elevation={0} className={classes.header}>
+          <Typography>{tutorialSteps[activeStep].label}</Typography>
+        </Paper>
         <AutoPlaySwipeableViews
           axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
           index={activeStep}
@@ -106,36 +101,31 @@ class SwipeableTextMobileStepper extends React.Component {
           enableMouseEvents
         >
           {tutorialSteps.map((step, index) => (
-            <div className={classes.imb} key={step.label}>
+            <div key={step.label}>
               {Math.abs(activeStep - index) <= 2 ? (
                 <img className={classes.img} src={step.imgPath} alt={step.label} />
               ) : null}
             </div>
           ))}
         </AutoPlaySwipeableViews>
-        <Paper square elevation={2} className={classes.header}>
-          <Typography variant='h5' component='h3'>{tutorialSteps[activeStep].label}</Typography>
-
-          <Typography variant='h6' paragraph>{'info'}</Typography>
-        </Paper>
-        {/* <MobileStepper
+        <MobileStepper
           steps={maxSteps}
           position="static"
           activeStep={activeStep}
           className={classes.mobileStepper}
           nextButton={
             <Button size="small" onClick={this.handleNext} disabled={activeStep === maxSteps - 1}>
-          Next
-          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+              Next
+              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
             </Button>
           }
           backButton={
             <Button size="small" onClick={this.handleBack} disabled={activeStep === 0}>
-          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-          Back
+              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+              Back
             </Button>
           }
-        /> */}
+        />
       </div>
     );
   }
@@ -147,3 +137,5 @@ SwipeableTextMobileStepper.propTypes = {
 };
 
 export default withStyles(styles, { withTheme: true })(SwipeableTextMobileStepper);
+
+
